2153a6dd1fed51869c0093c0e950a6fc2492d081
Niflan <niflanseven@gmail.com>
2025-02-21 11:21:15 +0530

    Added database schema

---
+++ CMJD-108-CRS/src/main/java/com/crs/db/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package main.java.com.crs.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/CRS";
+    private static final String USER = "root";
+    private static final String PASSWORD = "mysql";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
