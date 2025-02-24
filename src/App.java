634f28bae40bde90a19c6f61d3f0f2f2f4ac21f7
Niflan <niflanseven@gmail.com>
2025-02-24 14:34:06 +0530

    Course fxml file created and It is Link to App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
-public class App {
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello Java");
+        launch(args);
     }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL resource = getClass().getResource("./main/java/resources/com/view/course.fxml");
+        Parent root =FXMLLoader.load(resource);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+   
 }
--- src/main/java/com/crs/MainApp.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.crs;
-
-import javafx.application.Application;
-
-public class MainApp extends Application {
-    
-}
